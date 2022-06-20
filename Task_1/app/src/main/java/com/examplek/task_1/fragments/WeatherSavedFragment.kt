3ea2f1be193b9e509187a633feb538f171c7267a
Adwit Agrawal <102016125+adwit53@users.noreply.github.com>
2022-06-20 18:27:22 +0530

    Add files via upload

---
+++ Task_1/app/src/main/java/com/examplek/task_1/fragments/WeatherSavedFragment.kt
@@ -0,0 +1,8 @@
+package com.examplek.task_1.fragments
+
+import androidx.fragment.app.Fragment
+import com.examplek.task_1.R
+
+class WeatherSavedFragment : Fragment(R.layout.saved_weather) {
+
+}
---
+++ Task_1/app/src/main/java/com/examplek/task_1/fragments/WeatherSearchFragment.kt
@@ -0,0 +1,8 @@
+package com.examplek.task_1.fragments
+
+import androidx.fragment.app.Fragment
+import com.examplek.task_1.R
+
+class WeatherSearchFragment : Fragment(R.layout.search_weather) {
+
+}
